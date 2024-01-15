00714c61062efb1ed19b863425569adfe2b75f08
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2024-01-15 18:40:58 +0300

    automapper library projeye dahil edildi.

---
+++ Catalog/FreeCourse.Services.Catalog/Dtos/Mapping/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Dtos.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Course,CourseUpdateDto>().ReverseMap();
+            CreateMap<Course,CourseCreateDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+        }
+    }
+}
