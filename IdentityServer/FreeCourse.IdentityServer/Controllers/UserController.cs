adea7ecb78370b8f9ff57a984c72cd012e8de53c
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2024-03-23 21:55:09 +0300

    policiy eklendi.

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -1,16 +1,19 @@
 ﻿using FreeCourse.IdentityServer.Dtos;
 using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
+    [Authorize(LocalApi.PolicyName)]
     public class UserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
@@ -25,10 +28,11 @@ namespace FreeCourse.IdentityServer.Controllers
         {
             var user = new ApplicationUser { UserName = signUpDto.UserName, Email = signUpDto.Email };
             var res = await _userManager.CreateAsync(user, signUpDto.Password);
-            if (!res.Succeeded == false)
+            if (!res.Succeeded)
             {
                 return BadRequest(ResponseDTO<NoContent>.Fail(res.Errors.Select(x => x.Description).ToList(),400));
             }
+            
             return Ok();
         }
     }
